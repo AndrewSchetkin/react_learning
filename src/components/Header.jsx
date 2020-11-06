c95519d654be2bd6c6f3ee21b165ac2b1b2213b5
Андрей Щёткин <andrey.schetkin@gmail.com>
2020-11-06 21:53:00 +0300

    added scss

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+    return <header className="header">
+        <img src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/05/LOGO-r2.jpg?auto=format&q=60&fit=max&w=930" alt="" />
+    </header>
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,13 @@
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" alt="" />
+            </div>
+            <div></div>
+            <div></div>
+        </div>
+        );
+}
+
+export default Profile
